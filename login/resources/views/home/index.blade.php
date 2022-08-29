c3a906e7fa9f6742d902fca90791479128a0d3ae
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-08-29 14:58:10 -0500

    [UPDATE] Change lang to en

--- login/resources/views/home/index.blade.php
+++ login/resources/views/home/index.blade.php
@@ -5,10 +5,10 @@
 		@endauth
 		<h1>Home is where the heart is.</h1>
 		@auth
-		<p>Bienvenido {{auth()->user()->name ?? auth()->user()->user_name}}</p>
+		<p>Wellcome {{auth()->user()->name ?? auth()->user()->user_name}}</p>
 		@endauth
 		@guest
-		<p>Bienvenido, para ver el contenido debes autenticarte <a href="/login">Login</a> </p>
-		<p>Si aún no tienes una cuenta aquí la puedes crear <a href="/Register">Register</a> </p>
+		<p>Wellcome, please <a href="/login">Login here</a> if you want to see our content</p>
+		<p>Don't have an account yet? <a href="/Register">Register here</a> </p>
 		@endguest
 @endsection
